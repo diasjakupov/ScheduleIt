87f3f2640eb692e2c811846d4067b9b8cd6dd954
diasjakupov <dsjakupov@gmail.com>
2022-05-22 13:09:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/scheduleit/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.scheduleit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.scheduleit.ui.navigation.NavigationComposable
+import com.example.scheduleit.ui.navigation.TopBar
+import com.example.scheduleit.ui.theme.ScheduleItTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navHostController = rememberNavController()
+            ScheduleItTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Scaffold(
+                        topBar = {TopBar()}
+                    ) {
+                        NavigationComposable(navController = navHostController)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/scheduleit/ui/navigation/Navigation.kt
@@ -0,0 +1,18 @@
+package com.example.scheduleit.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.scheduleit.ui.main_screen.MainScreen
+
+
+@Composable
+fun NavigationComposable(navController: NavHostController){
+    NavHost(navController = navController, startDestination = "main_screen"){
+        composable("main_screen"){
+            MainScreen()
+        }
+    }
+}
